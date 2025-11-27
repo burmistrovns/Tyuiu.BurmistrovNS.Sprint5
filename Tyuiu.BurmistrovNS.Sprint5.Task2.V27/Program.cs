cba52ca7b6b73741d4850d6d90ac6bc45d886ffc
Николай Бурмистров <Tyuiu.BurmistrovNS@outlook.com>
2025-11-28 02:24:36 +0500

    Task2

---
+++ Tyuiu.BurmistrovNS.Sprint5.Task2.V27/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.BurmistrovNS.Sprint5.Task2.V27.Lib;
+namespace Tyuiu.BurmistrovNS.Sprint5.Task2.V27
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] matrix = new int[3, 3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"Введите элемент [{i},{j}]: ");
+                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            DataService ds = new DataService();
+            string path = ds.SaveToFileTextData(matrix);
+
+            Console.WriteLine("Результирующий массив:");
+            Console.WriteLine("***************************************************************************");
+
+            string[] lines = File.ReadAllLines(path);
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Файл с результатами: " + path);
+            Console.ReadKey();
+
+        }
+
+
+
+    }
+
+
+
+
+}
