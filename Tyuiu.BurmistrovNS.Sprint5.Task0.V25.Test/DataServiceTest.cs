43aae6b8fb79a93ace93a1a199f731d5847d5eb5
Николай Бурмистров <Tyuiu.BurmistrovNS@outlook.com>
2025-11-27 22:30:31 +0500

    Add project files.

---
+++ Tyuiu.BurmistrovNS.Sprint5.Task0.V25.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.BurmistrovNS.Sprint5.Task0.V25.Lib;
+using System.IO;
+
+namespace Tyuiu.BurmistrovNS.Sprint5.Task0.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestSaveToFileTextData()
+        {
+            DataService ds = new DataService();
+
+            int x = 3;
+            string path = ds.SaveToFileTextData(x);
+
+            Assert.IsTrue(File.Exists(path));
+
+            string result = File.ReadAllText(path);
+
+            
+            Assert.AreEqual("9,037", result);
+        }
+    }
+}
---
+++ Tyuiu.BurmistrovNS.Sprint5.Task0.V25/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.BurmistrovNS.Sprint5.Task0.V25.Lib;
+namespace Tyuiu.BurmistrovNS.Sprint5.Task0.V25
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 3;
+            string path = ds.SaveToFileTextData(x);
+
+            
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"Результат сохранен в файл: {path}");
+            Console.WriteLine("***************************************************************************");
+
+            
+            string result = File.ReadAllText(path);
+            Console.WriteLine($"Содержимое файла: {result}");
+            Console.WriteLine("***************************************************************************");
+
+            Console.ReadKey();
+
+
+
+        }
+
+
+
+    }
+
+
+
+
+}
