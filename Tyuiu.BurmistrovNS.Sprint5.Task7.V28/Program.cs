0c382044fa5f9535b0a636865259621dcc53d81c
Николай Бурмистров <Tyuiu.BurmistrovNS@outlook.com>
2025-11-29 02:49:06 +0500

    Task7

--- Tyuiu.BurmistrovNS.Sprint5.Task7.V28/Program.cs
+++ Tyuiu.BurmistrovNS.Sprint5.Task7.V28/Program.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.BurmistrovNS.Sprint5.Task7.V28
             Console.WriteLine("***************************************************************************");
 
             string path = Path.Combine(new string[] { "C:", "DataSprint5", "InPutDataFileTask7V28.txt" });
-            string pathSaveFile = Path.Combine(new string[] { "C:", "DataSprint5", "InPutDataFileTask7V28.txt" });
+            
 
             Console.WriteLine("Данные находтся в файле: " + path);
 
@@ -21,8 +21,8 @@ namespace Tyuiu.BurmistrovNS.Sprint5.Task7.V28
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            pathSaveFile = ds.LoadDataAndSave(pathSaveFile);
-            Console.WriteLine(pathSaveFile);
+            path = ds.LoadDataAndSave(path);
+            Console.WriteLine(path);
 
 
 
